54e960ee666874d96548ad7b54ae53d6668dcbfb
Thamarai <tmanalan710@gmail.com>
2025-06-11 18:23:29 +0530

    Card Initialized

--- Components/Card.tsx
+++ Components/Card.tsx
@@ -1,5 +1,18 @@
-import content from '@/Content/content.json'
+import { useEffect, useState } from 'react'
+interface Data{
+name:string,
+points:number,
+image?:string,
+link:string
+}
+const [content,setContent] = useState<Data[]>([])
 export function Card(){
+    console.log(content)
+    useEffect(()=>{
+        fetch("/content.json")
+        .then((res)=>res.json())
+        .then((data)=>setContent(data))
+    },[])
     console.log(content)
     return(
         <section>

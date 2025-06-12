7c593b6ac0c69a8e285d504a9773ed740f4f70b2
Thamarai <tmanalan710@gmail.com>
2025-06-12 09:04:41 +0530

    Processing

--- Components/Card.tsx
+++ Components/Card.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { error } from 'console'
 import { useEffect, useState } from 'react'
 interface Data{
 name:string,
@@ -5,18 +7,33 @@ points:number,
 image?:string,
 link:string
 }
-const [content,setContent] = useState<Data[]>([])
+type DataGroup = {
+    [key: string]: Data[]
+}
+
 export function Card(){
+    const [content,setContent] = useState<DataGroup>({})
     console.log(content)
     useEffect(()=>{
         fetch("/content.json")
         .then((res)=>res.json())
         .then((data)=>setContent(data))
+        .catch((err)=>{console.error("The Fetch is failed")})
     },[])
-    console.log(content)
+    console.log(content.toString())
     return(
-        <section>
-             
-        </section>
+        <div>
+            {Object.entries(content).map(([groupName , alternatives])=>(
+                <div>
+                    <div className='text-white'>{groupName}</div>
+                    {alternatives.map((alter)=>
+                    (
+                        <div key={alter.name} className='text-white'>
+                            {alter.name}
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </div>
     )
 }
